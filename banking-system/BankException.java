be65543c8fff575795633cf6513d028fe9c56c88
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-02-15 10:58:47 +0530

    Update BankException.java

--- banking-system/BankException.java
+++ banking-system/BankException.java
@@ -1,8 +1,33 @@
 
 package bankingSystem;
 
-public class BankException extends Exception{
-    public BankException(String message){
-        super(message);
+public class BankException extends Exception {
+
+    public static class InvalidAccountException extends BankException {
+        public InvalidAccountException() {
+            System.out.println("Account Number in not Valid.It should contain 12 digits");
+        }
+    }
+
+    // NegativeAmountException nested class
+    public static class NegativeAmountException extends BankException {
+        public NegativeAmountException() {
+            System.out.println("Amount cannot be negative");
+        }
+    }
+
+    //InsufficientFundsException
+    public static class InsufficientFundsException extends BankException {
+        public InsufficientFundsException() {
+            System.out.println("Insufficient Balance");
+        }
+    }
+
+    //NonExistenceAccountException
+    public static class NonExistenceAccountException extends BankException {
+        public NonExistenceAccountException() {
+            System.out.println("The Account does not exists");
+        }
     }
 }
+
