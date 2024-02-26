bbcbc16f8624f04af2ec32f5cda0d07f7908dc1b
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-02-26 12:39:48 +0530

    Update Main.java

--- WeeklyAssignment4/Main.java
+++ WeeklyAssignment4/Main.java
@@ -8,11 +8,11 @@ public class Main {
         // Paths for text files and XML files to be compared
         String textFile1 = "E:/txtfile1.txt";
         String textFile2 = "E:/txtfile2.txt";
-        String xmlFile1 = "E:/file1.xml";
-        String xmlFile2 = "E:/file2.xml";
+      //  String xmlFile1 = "E:/file1.xml";
+        //String xmlFile2 = "E:/file2.xml";
         // Compare text files
         f.compareTextFiles(textFile1,textFile2);
         // Compare XML files
-        f.compareXMLFiles(xmlFile1, xmlFile2);
+      //  f.compareXMLFiles(xmlFile1, xmlFile2);
     }
 }
