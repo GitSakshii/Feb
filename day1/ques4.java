00625819b744e6c26ba8da18e3700dbf8ec8effa
GitSakshii <157839341+GitSakshii@users.noreply.github.com>
2024-02-28 14:05:15 +0530

    Update ques4.java

--- day1/ques4.java
+++ day1/ques4.java
@@ -5,38 +5,54 @@ public class ques4 {
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
+        // Input two strings
         String a=sc.nextLine();
         String b=sc.nextLine();
+        // Convert Strings to char arrays
         char A[]=a.toCharArray();
         char B[]=b.toCharArray();
+        // Get lengths of input strings
         int n=a.length();
         int m=b.length();
-        int ans=0; int pow=1;
 
+        int ans=0; // Initialize the result
+        int pow=1; // Initialize the power of 10
 
+        // Iterate through each digit of the first number (from right to left)
         for(int i=n-1;i>=0;i--)
         {
-            int prod=0;
-            int pr=0;
-            int p=1;
-            int cary=0;
+            int prod=0;// Initialize the product
+            int pr=0;// Initialize the partial product
+            int p=1;// Initialize the power of 10 for positionin
+            int cary=0;// Initialize the carry
             int r=0;
+            // Iterate through each digit of the second number (from right to left)
             for(int j=m-1;j>=0;j--)
             {
+                // Multiply the digits and add the carry
                  pr=(A[i]-'0')*(B[j]-'0');
                  r=pr+cary;
+                // Update carry and remainder
                  cary=r/10;
                  r=r%10;
+                // Add the current digit to the product
                 prod=prod+(r*p);
                 p=p*10;
             }
             r=cary;
+            // Add the carry to the product
             prod=prod+r*p;
-           ans= ans+prod*pow;
+            // Add the current partial product to the final result
+
+            ans= ans+prod*pow;
+            // Update the positioning factor
             pow=pow*10;
 
         }
+        // Print the final result
         System.out.println(ans);
 
     }
 }
+
+   
